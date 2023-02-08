8fa7827daedd2a6871e0a385f2de24019df56709
Artem <102421671+YL-bot@users.noreply.github.com>
2023-02-08 18:54:10 +0300

    Create district.py

---
+++ district.py
@@ -0,0 +1,52 @@
+import sys
+from io import BytesIO
+# Этот класс поможет нам сделать картинку из потока байт
+import requests
+from PIL import Image
+import function_file
+
+
+# Пусть наше приложение предполагает запуск:
+# python search.py Москва, ул. Ак. Королева, 12
+# Тогда запрос к геокодеру формируется следующим образом:
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+if not response:
+    # обработка ошибочной ситуации
+    pass
+
+a = response.json()
+finx, finy = function_file.coord(a)
+
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+# Получаем первый топоним из ответа геокодера.
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+# Координаты центра топонима:
+toponym_coodrinates = toponym["Point"]["pos"]
+# Долгота и широта:
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": ",".join([toponym_longitude, toponym_lattitude]),
+    'kind': 'district',
+    'results': '1',
+    'format': 'json'
+}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+print(response.json()["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]['name'])
